7dabb37ae9855806caa2c9dbfc9a11bda002f6f8
Shaishav Gandhi <shaishav@uber.com>
2020-04-26 12:27:23 -0700

    Release 0.4.2

--- Formula/fab.rb
+++ Formula/fab.rb
@@ -4,9 +4,9 @@
 class Fab < Formula
   desc "Command-line utilities for working with Phabricator"
   homepage "https://github.com/ShaishavGandhi/fab"
-  url "https://github.com/ShaishavGandhi/fab/releases/download/v0.4.1/fab-v041.tar.gz"
-  sha256 "0fd0b7a42223cea1419b82ad3a0893a801dfb45f07d4aecb14dd9ab6d4bf0d9f"
-  version "0.4.1"
+  url "https://github.com/ShaishavGandhi/fab/releases/download/v0.4.2/fab-v042.tar.gz"
+  sha256 "b16634adc6fc27a7a33e83dc886f65c256d8a0ff671b9249adc2175800c48adb"
+  version "0.4.2"
 
   def install
     bin.install "fab"
